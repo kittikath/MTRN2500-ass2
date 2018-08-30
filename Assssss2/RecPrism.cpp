5b7ea37479df0e08b1a1cfd5743bf9f29a75f802
kath <ikath@hotmail.com>
2018-08-30 15:02:57 +1000

    First commit

---
+++ Assssss2/RecPrism.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "RecPrism.h"
+
+#include <Windows.h>
+#include <tchar.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+
+
+using namespace std;
+RecPrism::RecPrism(float x, float y, float z) {
+	xl = x;
+	yl = y;
+	zl = z;
+}
+
+void RecPrism::draw() 
+{
+	glPushMatrix();
+	//glRotated(45, 0, 0, 1); //angle, x, y, z, true or false
+	//glTranslated(10, 0, 0);
+	
+	//bottom face :)
+	glBegin(GL_QUADS);
+		glVertex3f(-xl / 2, 0, zl / 2);
+		glVertex3f(xl/2, 0, zl/2);
+		glVertex3f(xl/2, 0, -zl/2);
+		glVertex3f(-xl/2, 0, -zl/2);
+	glEnd();
+
+	// top face
+	
+	glBegin(GL_QUADS);
+		glVertex3f(-xl / 2, yl, zl / 2);
+		glVertex3f(xl / 2, yl,  zl / 2);
+		glVertex3f(xl / 2, yl, -zl / 2);
+		glVertex3f(-xl / 2, yl, -zl / 2);
+	glEnd();
+	
+
+	glPopMatrix();
+	
+
+	//need to reset to reset coordinate system
+	//can reverse functions done previously
+	//or store the original coordinate system in the stack and then push it on to the stack and remove from the stack after editing
+	//use push and pop
+
+	//TO DO: 5 additional + header files (one for each dshape and vehicle) and then be able to draw each shape
+
+
+}
---
+++ Assssss2/RecPrism.h
@@ -0,0 +1,25 @@
+#ifndef RECPRISM_H
+#define RECPRISM_H
+#endif RECPRISM_H
+
+class RecPrism 
+{
+
+private:
+	//set values
+	float xl;
+	float yl;
+	float zl;
+
+public:
+	RecPrism();
+	RecPrism(float x, float y, float z);
+	~RecPrism() = default;
+	void draw();
+	/*double GetX();
+	double GetY();
+	double GetZ();
+	void SetX(double xx);
+	void SetY(double yy);
+	void SetZ(double zz);*/
+}; 
